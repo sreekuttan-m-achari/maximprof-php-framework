3318327a255539b0b260935393c339d7479bf330
Sreekuttan M Achari <sreekuttan@maximprof.com>
2020-05-22 13:29:39 +0530

    added auto loader

---
+++ autoload.php
@@ -0,0 +1,28 @@
+<?php
+include_once __DIR__ . '/config/config.php';
+include_once __DIR__ . '/lib/helpers.php';
+include_once __DIR__ . '/lib/auth.php';
+//include_once __DIR__ . '/lib/smtp-mail.php';
+//include_once __DIR__ . '/lib/pdf-converter.php';
+
+spl_autoload_register('load_library');
+spl_autoload_register('load_model');
+
+function load_library($class_name) {
+	$path = __DIR__ . '/lib/';
+	$extention = '.class.php';
+	$full_path = $path . $class_name . $extention;
+
+	if (file_exists($full_path)) {
+		include_once $full_path;
+	}
+}
+
+function load_model($class_name) {
+	$path = __DIR__ . '/models/';
+	$extention = '.class.php';
+	$full_path = $path . $class_name . $extention;
+	if (file_exists($full_path)) {
+		include_once $full_path;
+	}
+}
